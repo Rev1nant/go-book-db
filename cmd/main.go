333a507d5de946e73fac27ca8d01a0c428324172
Yaroslav Kochin <YaroslavKochin43@gmail.com>
2025-09-03 17:11:20 +0500

    Added handlers Author and Genre

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/Rev1nant/go-book-db/configs"
+	"github.com/Rev1nant/go-book-db/internal/handler"
 	"github.com/Rev1nant/go-book-db/internal/repository"
 	"github.com/Rev1nant/go-book-db/internal/server"
 	"github.com/Rev1nant/go-book-db/internal/service"
 	"github.com/Rev1nant/go-book-db/pkg/db"
 )
 
-func main() {
+func App() http.Handler {
 	conf := configs.LoadConfig("../.env")
 	dsn := conf.DB.Dsn
 	db := db.NewDB(dsn)
-	defer db.DB.Close()
 
 	repo := repository.NewRepositories(db)
 	serv := service.NewServices(repo)
+	router := http.NewServeMux()
+	handler.NewAuthorHandler(router, handler.AuthorHandlers{
+		AuthorService: serv.Author,
+	})
 
+	handler.NewGenreHendler(router, handler.GenreHandlers{
+		GenreService: serv.Genre,
+	})
+
+	return router
+}
+
+func main() {
 	server := server.Server{}
-	server.Run("8080")
+	server.Run("8080", App)
 }
